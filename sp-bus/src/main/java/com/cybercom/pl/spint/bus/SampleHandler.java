7f69f752d855c31cac26f4b2b3daa2cceee927c7
Michal Jedryszka <michal@jedryszka.pl>
2014-11-12 16:08:17 +0100

    message passing to main bus component

---
+++ sp-bus/src/main/java/com/cybercom/pl/spint/bus/SampleHandler.java
@@ -0,0 +1,8 @@
+package com.cybercom.pl.spint.bus;
+
+public class SampleHandler {
+	
+	public String handle(String data){
+		return "[bus: " + data + "]";
+	}
+}
--- sp-request/src/main/java/com/cybercom/pl/spint/request/RequestController.java
+++ sp-request/src/main/java/com/cybercom/pl/spint/request/RequestController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -14,10 +15,9 @@ public class RequestController {
 	private RequestService requestService;
 
 	@RequestMapping(value = "/hello", method = RequestMethod.GET)
-	public String hello(ModelMap model) {
-		String message = "JCG Hello World!";
+	public String hello(ModelMap model, @RequestParam String message) {
+		message = requestService.hello(message);
 		model.addAttribute("msg", message);
-		return requestService.hello(message);
+		return message;
 	}
-
 }
